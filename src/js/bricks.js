a971550ea1edbf2ec61020c1ae62dd0fe3a46032
Artem Safin <artemsafin.webdev@gmail.com>
2022-07-01 14:22:38 +0300

    Привязал модалку

--- src/js/bricks.js
+++ src/js/bricks.js
@@ -3,13 +3,16 @@ import { primaryInput } from 'detect-it';
 export default function bricks() {
     if (primaryInput === 'touch') return;
 
-    
+
     const elements = Array.from(document.querySelectorAll('.bricks'));
 
     elements.forEach(element => {
+
+        let tracking = false;
         const cursor = element.querySelector('.bricks__cursor');
 
         element.parentElement.addEventListener('mousemove', event => {
+            if (!tracking) return;
             console.log(event);
 
             element.parentElement.style.setProperty('--x', event.clientX - element.getBoundingClientRect().left + 'px');
@@ -17,9 +20,11 @@ export default function bricks() {
         });
 
         element.parentElement.addEventListener('mouseenter', () => {
+            tracking = true;
             cursor.classList.add('visible');
         });
         element.parentElement.addEventListener('mouseleave', () => {
+            tracking = false;
             cursor.classList.remove('visible');
         });
     });
