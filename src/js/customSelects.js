4dd3bf0cf8f4faf7cd4583d9c1194a7685d6b82c
Artem Safin <artemsafin.webdev@gmail.com>
2022-09-15 14:34:36 +0300

    По задачам

--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -1,38 +1,43 @@
 import Choices from 'choices.js';
 
 export default function customSelects() {
-    const customSelects = Array.from(document.querySelectorAll('.js-custom-select'));
-
-    customSelects.forEach(select => {
-        const parentForm = select.closest('form');
-        const instance = new Choices(select, {
-            searchEnabled: false,
-            itemSelectText: '',
-            shouldSort: false
-        });
-
-        const defaultValue = instance.getValue(true);
-
-        console.log(defaultValue)
-
-        instance.passedElement.element.addEventListener(
-            'choice',
-            () => {
-                setTimeout(() => {
-                    $(instance.passedElement.element)
-                        .parsley()
-                        .validate();
-                }, 100);
-            },
-            false
-        );
-
-        if (parentForm) {
-            parentForm.addEventListener('reset', () => {
-                console.log('Parent form reset');
-
-                instance.setChoiceByValue(defaultValue);
+    function initializeSelects() {
+        const customSelects = Array.from(document.querySelectorAll('.js-custom-select'));
+        customSelects.forEach(select => {
+            const parentForm = select.closest('form');
+            const instance = new Choices(select, {
+                searchEnabled: false,
+                itemSelectText: '',
+                shouldSort: false
             });
-        }
-    });
+
+            const defaultValue = instance.getValue(true);
+
+            console.log(defaultValue);
+
+            instance.passedElement.element.addEventListener(
+                'choice',
+                () => {
+                    setTimeout(() => {
+                        $(instance.passedElement.element)
+                            .parsley()
+                            .validate();
+                    }, 100);
+                },
+                false
+            );
+
+            if (parentForm) {
+                parentForm.addEventListener('reset', () => {
+                    console.log('Parent form reset');
+
+                    instance.setChoiceByValue(defaultValue);
+                });
+            }
+        });
+    }
+
+    initializeSelects();
+
+    window.initializeSelects = initializeSelects;
 }
